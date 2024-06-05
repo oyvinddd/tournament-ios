65e6fdbc4256ad1f814f9372e9bc39f198203202
Øyvind Hauge <oyvind.s.hauge@gmail.com>
2024-06-06 00:18:01 +0200

    adding/removing shit

---
+++ Tournament/Screens/Tournament/Views/TournamentDetailsView.swift
@@ -0,0 +1,21 @@
+//
+//  TournamentDetailsView.swift
+//  Tournament
+//
+//  Created by Øyvind Hauge on 06/06/2024.
+//
+
+import SwiftUI
+
+struct TournamentDetailsView: View {
+    
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TournamentDetailsView()
+}
